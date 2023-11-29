9c4064f88fbff66912497c6a765dcebd9ce5cdef
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-11-29 16:58:09 -0300

    feito o put para registrar  jogadores

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from database.jogador_db import JogadorDB
 from controller.jogador_controller import JogadorController
 app = FastAPI()
 
-
+@app.put("/registrar/jogadores/{nome}/{email}/{senha}")
+def registrar_jogadores(nome: str, email : str, senha: str):
+    return JogadorController.get_instance().inserir_jogadores_banco(nome, email, senha)
 
 @app.get("/listar/jogadores")
 async def lista_jogadores():
