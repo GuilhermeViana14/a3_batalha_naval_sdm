cbfe3e152a67f13c35179a1dacc0444a7b06e8f9
pedrofleckk <pedrofleckrex@gmail.com>
2023-11-29 19:12:35 -0300

    arquivo Login_controller.py criado

---
+++ batalha_naval/controller/login_controller.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, HTTPException
+from .jogadorDB import JogadorDB
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class User(BaseModel):
+    nome: str
+    senha: str
+
+@router.post("/login")
+async def login(user: User):
+    jogador_db = JogadorDB()
+
+    # Verifica se as credenciais existem no banco de dados
+    if jogador_db.verificar_credenciais_por_nome_e_senha(user.nome, user.senha):
+        return {"message": "Login feito com sucesso!"}
+    else:
+        raise HTTPException(status_code=401, detail="Nome ou Senha inválido.")
