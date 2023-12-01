ac89c03cf37fcf19ec397f69b92bb12df46342e8
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-11-30 22:00:02 -0300

    aletrando o autenticacao_db

--- batalha_naval/database/autenticacao_db.py
+++ batalha_naval/database/autenticacao_db.py
@@ -1,5 +1,7 @@
 from model.autenticacao import Autenticacao
 from datetime import datetime, timedelta
+import hashlib
+
 
 class AutenticacaoDB:
     
@@ -13,5 +15,16 @@ class AutenticacaoDB:
         self.sala = [s for s in self.sala if s.nome != nome]
     
     def criar_sala(self, nome : str):
+        self.deletar_sala(nome)
+        
+        tempo = datetime.now()
+        tempo = tempo + timedelta(minutes=10)
+        
+        id = hashlib.sha1((nome + str(tempo.timestamp())).encode('utf'))
+        
+        sessao : Autenticacao = Autenticacao(nome, id.hexdigest(), tempo)
+        self.sala.append(sessao)
+        
+        return sessao.id
         
         
