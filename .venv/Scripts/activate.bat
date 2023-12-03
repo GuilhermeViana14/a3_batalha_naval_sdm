a7f9a0880dccca0c6fda1c8aebd000bd9d82aaea
guilhermeviana14 <guilhermevianafilgueiras@gmail.com>
2023-12-03 19:41:33 -0300

    instalado a venv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Windows\Desktop\A3-SDM\.venv
+set VIRTUAL_ENV=C:\Users\Windows\Desktop\Nova pasta\a3_batalha_naval_sdm\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
